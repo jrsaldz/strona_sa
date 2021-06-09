2f946b7e37e30d7025434535508c11e2d19a4ec1
jrsaldz <jrsaldz@localhost>
2021-06-10 00:47:14 +0200

    dodanie widoku ze spisem mityngów

--- strona_sa/urls.py
+++ strona_sa/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from mityngi.views import MityngListView
+
 urlpatterns = [
+    path('', MityngListView.as_view()),
     path('admin/', admin.site.urls),
 ]
