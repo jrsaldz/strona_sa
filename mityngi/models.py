73758810971366315e7c1393c118338ec631e246
jrsaldz <jrsaldz@localhost>
2021-06-10 00:14:42 +0200

    dodanie modeli

--- mityngi/models.py
+++ mityngi/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+
+class DzienTygodnia(models.Model):
+    nazwa = models.CharField(max_length=15)
+    slug = models.SlugField(max_length=15)
+
+
+class Miasto(models.Model):
+    nazwa = models.CharField(max_length=40)
+    slug = models.SlugField(max_length=40)
+
+
+class Mityng(models.Model):
+    nr_telefonu = models.CharField(max_length=20, blank=False)
+    pin = models.CharField(max_length=10, blank=True)
+    godzina = models.PositiveSmallIntegerField()
+    minuta = models.PositiveSmallIntegerField()
+    dzien_tygodnia = models.ForeignKey(DzienTygodnia, on_delete=models.CASCADE)
+    miasto = models.ForeignKey(Miasto, on_delete=models.CASCADE)
