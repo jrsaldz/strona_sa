50a29f135a16ca1b39a546eedaf550761c37d4d9
jrsaldz <jrsaldz@localhost>
2021-06-10 00:26:08 +0200

    dodanie nazw wyświetlanych

--- mityngi/models.py
+++ mityngi/models.py
@@ -7,10 +7,16 @@ class DzienTygodnia(models.Model):
     nazwa = models.CharField(max_length=15)
     slug = models.SlugField(max_length=15)
 
+    def __str__(self):
+        return self.nazwa
+
 
 class Miasto(models.Model):
     nazwa = models.CharField(max_length=40)
     slug = models.SlugField(max_length=40)
+
+    def __str__(self):
+        return self.nazwa
 
 
 class Mityng(models.Model):
@@ -20,3 +26,6 @@ class Mityng(models.Model):
     minuta = models.PositiveSmallIntegerField()
     dzien_tygodnia = models.ForeignKey(DzienTygodnia, on_delete=models.CASCADE)
     miasto = models.ForeignKey(Miasto, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{} - {} {:02}:{:02}'.format(self.miasto, self.dzien_tygodnia, self.godzina, self.minuta)
